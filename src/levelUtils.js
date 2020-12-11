3b287fc629b2c6b10f8117604a277d8a4f613bc0
Ezerous <ezerous@gmail.com>
2020-12-11 20:38:18 +0200

    Make sure database is closed properly

--- src/levelUtils.js
+++ src/levelUtils.js
@@ -3,20 +3,25 @@ import level from 'level';
 /* Used in development only to store the identity.signatures.publicKey so developers don't have to
 repeatedly sign theOrbitDB creation transaction in MetaMask when React development server reloads
 the app */
-const ethProviderDB = level('ethprovider/identities');
+const ethProviderDBName = 'ethprovider/identities';
 
 async function storeIdentitySignaturePubKey(key, signaturePubKey) {
+  const ethProviderDB = level(ethProviderDBName);
   await ethProviderDB.put(key, signaturePubKey);
+  ethProviderDB.close();
 }
 
 // If it exists, it returns the identity.signatures.publicKey for the given key (key is the
 // concatenation of identity.publicKey + identity.signatures.id)
 async function getIdentitySignaturePubKey(key) {
+  const ethProviderDB = level(ethProviderDBName);
   try {
     return await ethProviderDB.get(key);
   } catch (err) {
     if (err && err.notFound) return null; // Not found
     throw err;
+  } finally {
+    ethProviderDB.close();
   }
 }
 
